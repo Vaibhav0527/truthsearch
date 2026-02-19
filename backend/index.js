460b7af13669f0021a53d11c63b5829d8d82b3e2
SHIVA00202 <shivayadav1238800@gmail.com>
2026-02-19 23:56:16 +0530

    feat(auth): implement user authentication with JWT

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,11 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
-import connectCloudinary from "./config/cloudinary.js";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/authroutes.js"
+import userRouter from "./routes/userroutes.js"
+
+import uploadOnCloudinary from "./utils/cloudinary.js";
 
 
 
@@ -14,10 +18,16 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(cookieParser());
 
 
 connectDB();
-connectCloudinary();
+uploadOnCloudinary();
+
+
+app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
+
 
 app.get("/", (req, res) => {
   res.send("API WORKING");
---
+++ backend/models/usermodel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+
+    },
+    mobile: {
+        type: String,
+        required: true
+    },
+    
+    resetOtp: {
+        type: String,
+    },
+    isOtpVerified: {
+        type: Boolean,
+        default: false
+    },
+    otpExpires: {
+        type: Date
+    }
+   
+}, { timestamps: true })
+userSchema.index({ location: "2dsphere" });
+
+const User = mongoose.model("User", userSchema);
+export default User;
