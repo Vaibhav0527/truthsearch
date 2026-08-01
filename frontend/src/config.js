621e91a7480083d7e0370cd63b3baac570ddd860
Vaibhav Gupta <vaibhav0527gupta@gmail.com>
2026-08-02 02:10:28 +0530

    Update tunnel URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const serverUrl = import.meta.env.VITE_SERVER_URL || "https://notify-building-pitch-babies.trycloudflare.com";
+export const serverUrl = import.meta.env.VITE_SERVER_URL || "https://metal-maximum-hart-wireless.trycloudflare.com";
 export const backendUrl = import.meta.env.VITE_BACKEND_URL || "https://truthsearch-backend-production.up.railway.app";
